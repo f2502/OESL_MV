08239e98e44ff1791038780f0a5c053732982ad0
f2502 <frieder.fueger1@tu-dresden.de>
2023-07-11 11:10:04 +0200

    f

--- bhd.R
+++
@@ -1,5 +0,0 @@
-﻿#---------------- Funktion BHD klassifizieren-----------------#
-
-classifyBHD <- function(bhd) {
-  cut(bhd, breaks = c(0, 7, 20, 35, 50, 99, Inf), labels = c(1:6), right = FALSE)
-}
--- vorrat.R
+++
@@ -1,6 +0,0 @@
-﻿
-#---------------- Funktion Vorrat klassifizieren--------------#
-
-classifyvor <- function(vor) {
-  cut(vor, breaks = c(0,50, 150,250,500, Inf), labels = c(111:115), right = FALSE)
-}
