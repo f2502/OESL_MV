4c57e582f14cd925d0f0905b8ac6add0b12585ae
f2502 <frieder.fueger1@tu-dresden.de>
2023-07-11 11:11:13 +0200

    Update klassifizierung.R

--- klassifizierung.R
+++ klassifizierung.R
@@ -9,5 +9,16 @@ classifyAlter <- function(alter) {
 }
 
 
+ #---------------- Funktion BHD klassifizieren-----------------#
+  
+  classifyBHD <- function(bhd) {
+    cut(bhd, breaks = c(0, 7, 20, 35, 50, 99, Inf), labels = c(1:6), right = FALSE)
+  }
 
 
+
+  #---------------- Funktion Vorrat klassifizieren--------------#
+  
+  classifyvor <- function(vor) {
+    cut(vor, breaks = c(0,50, 150,250,500, Inf), labels = c(111:115), right = FALSE)
+  }
